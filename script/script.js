08e94427934cabe1b70a0cc3677fcb6be7a64fab
Richard Duffy <rduffy@slack-corp.com>
2021-12-01 19:53:17 +0000

    Removed dependency on id, in case the page needs similar logic later, and id's shouldn't be duplicated

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ let groceries=[];
 function loader(){
     let buildList = "";
     for (i=0;i<groceries.length;i++) {
-        buildList = buildList + "<li><div class='row justify-content-between'><p class='col-3'>"+groceries[i]+"</p><button class='closer verticals buttons col-1 ps-2' id="+i+" onclick='removeItem(this.id)'>x</button></div></li>";
+        buildList = buildList + "<li><div class='row justify-content-between'><p class='col-3'>"+groceries[i]+"</p><button class='closer verticals buttons col-1 ps-2' onclick='removeItem("+i+")'>x</button></div></li>";
     }
     document.getElementById("list").innerHTML = buildList + "<li><form class='row justify-content-between ps-2'><input type='text' id='newItem' class='col-6 verticals'><input type='submit' value='Add' class='buttons verticals col-2' onclick='addItem()'></input></form></li>";
     document.getElementById("newItem").focus();
